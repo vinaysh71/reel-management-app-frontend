9422e7627ff259f131d9bb06c4cdb57812613da1
Vinay Sharma <vinn.sharma@yahoo.com>
2026-01-26 18:47:47 +0530

    clean up

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,9 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export async function getDashboardData() {
-  const res = await fetch(`${process.env.BASE_URL}/dashboard/kpis`);
-  const data = await res.json();
-  return data;
-}
